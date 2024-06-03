fd808d5609db5628e608a2ce4a384ce1a21aac10
borisbolliet <boris.bolliet@gmail.com>
2024-06-03 23:58:23 +0100

    credits

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup,find_packages
 
 setup(
     name="classy_szfast",
-    version="0.0.2",
+    version="0.0.3",
     description="Python package for fast class_sz",
     zip_safe=False,
     packages=find_packages(),
-    author='Boris Bolliet et al',
-    author_email='boris.bolliet@gmail.com',
+    author="Boris Bolliet, Ola Kusiak",
+    author_email="bb667@cam.ac.uk, akk2175@columbia.edu",
     url='https://github.com/CLASS-SZ/classy_szfast',
     download_url='https://github.com/CLASS-SZ/classy_szfast',
     package_data={},
