b705b4f46cdb322beae4a7a4827e2b8f23229d2d
borisbolliet <boris.bolliet@gmail.com>
2024-06-04 22:18:06 +0100

    pypi update no hardcoded paths

--- classy_szfast/config.py
+++ classy_szfast/config.py
@@ -1,1 +1,6 @@
-path_to_cosmopower_organization = '/Users/boris/Work/CLASS-SZ/SO-SZ/class_sz/cosmopower-organization'
+import os
+
+def get_cosmopower_path():
+    return os.getenv('PATH_TO_COSMOPOWER_ORGANIZATION')
+
+path_to_cosmopower_organization = get_cosmopower_path()
--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         'Intended Audience :: Science/Research'
     ],
     name="classy_szfast",
-    version="0.0.7",
+    version="0.0.8",
     description="Python package for fast class_sz",
     # long_description=long_description,
     long_description_content_type='text/markdown',
