6cd0f0ac26dea3c3f73b2e69548cb7932e99c749
borisbolliet <boris.bolliet@gmail.com>
2024-06-04 01:18:23 +0100

    vers

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup,find_packages
 
 setup(
     name="classy_szfast",
-    version="0.0.3",
+    version="0.0.5",
     description="Python package for fast class_sz",
     zip_safe=False,
     packages=find_packages(),
